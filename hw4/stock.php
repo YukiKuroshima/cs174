3f2fe0420b86ed25ae966ab0feed77c12b64bc5d
YukiKuroshima <yukikuro1993@gmail.com>
2018-04-19 08:54:02 -0700

    Add doc

---
+++ hw4/stock.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <form action="stock.php" method="get">
+        Company Name or Symbol: <input type="text" name="name">
+        <br>
+        <input type="submit" value="Search"><input type="submit" value="Clear">
+    </form>
+</body> 
+</html>
