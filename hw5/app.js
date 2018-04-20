6d59b1c8c9d3e709274637995da4d3fb8541961a
YukiKuroshima <yukikuro1993@gmail.com>
2018-04-19 21:07:55 -0700

    :tada: Done

--- hw5/app.js
+++ hw5/app.js
@@ -12,7 +12,77 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/twitterRest.html'));
 });
 
-app.get('/list',function(req,res){
+/*
+ * Endpoint for showing the list of tweets
+ */
+app.get('/tweets/:tid?',function(req,res){
+  res.setHeader('Content-Type', 'application/json');
+  fs.readFile('favs.json', 'utf8', function(err, contents) {
+    const tweets = JSON.parse(contents);
+
+    const tid = req.params.tid;
+
+    var result = [];
+
+    for (var i = 0; i < tweets.length; i++) {
+      const tweet = tweets[i];
+      const elem = {
+        "created_at": tweet.created_at,
+        "id": tweet.id,
+        "text": tweet.text
+      };
+      // if tweet id is defined
+      if (tid) {
+        // check if tweet id is same
+        if (elem.id == tid) {
+          result[i] = elem;
+        }
+      } else {
+        result[i] = elem;
+      }
+    }
+
+    res.send(JSON.stringify(result));
+  });
+});
+
+/*
+ * Endpoint for showing the list of users
+ */
+app.get('/users/:sn?',function(req,res){
+  res.setHeader('Content-Type', 'application/json');
+  fs.readFile('favs.json', 'utf8', function(err, contents) {
+    const tweets = JSON.parse(contents);
+
+    const sn = req.params.sn;
+    var result = [];
+
+    for (var i = 0; i < tweets.length; i++) {
+      const tweet = tweets[i];
+      const elem = {
+        "name": tweet.user.name,
+        "screen_name": tweet.user.screen_name,
+        "location": tweet.user.location,
+        "description": tweet.user.description
+      };
+      // if screen name is defined
+      if (sn) {
+        // check if screen name is same
+        if (elem.screen_name == sn) {
+          result[i] = elem;
+        }
+      } else {
+        result[i] = elem;
+      }
+    }
+    res.send(JSON.stringify(result));
+  });
+});
+
+/*
+ * Endpoint for showing the list of users
+ */
+app.get('/links',function(req,res){
   res.setHeader('Content-Type', 'application/json');
   fs.readFile('favs.json', 'utf8', function(err, contents) {
     const tweets = JSON.parse(contents);
@@ -21,20 +91,45 @@ app.get('/list',function(req,res){
 
     for (var i = 0; i < tweets.length; i++) {
       const tweet = tweets[i];
-      // console.log(tweet);
+      const links = getAllUrls(tweet);
+
       const elem = {
-        "created_at": tweet.created_at,
         "id": tweet.id,
-        "text": tweet.text
+        "links": links
       };
       result[i] = elem;
     }
-
-    console.log(result);
     res.send(JSON.stringify(result));
   });
 });
 
+/*
+ * Iterete through the json
+ * return a list of urls
+ */
+function getAllUrls(tweet) {
+  var urls = [];
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  const regex = new RegExp(expression);
 
+  for(var key in tweet){
+    const value = tweet[key];
+    // null -> skip
+    if (value == null) continue;
+
+    // check if current value is object
+    if (typeof value === "object") {
+      // recursive call
+      urls.push(...getAllUrls(value));
+    } else {
+      // check if current value is url
+      const match = value.toString().match(regex);
+      if (match) {
+        urls.push(match[0]);
+      }
+    }
+  }
+  return urls;
+} 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- hw5/public/show.js
+++ hw5/public/show.js
@@ -1,28 +1,90 @@
-console.log("show js");
-
+/*
+ * Triggered when user clicks a button
+ */
 function show (name) {
-  console.log("show: " + name);
-  loadJSON(name);
-
+  var param = "";
+  if (name === "tweets") {
+    param = document.getElementById("tweetId").value;
+    document.getElementById("tweetId").value = "";
+  } else if (name === "users") {
+    param = document.getElementById("screenName").value;
+    document.getElementById("screenName").value = "";
+  }
+  loadJSON(name, param);
 }
 
 
-function loadJSON (url) {
+/*
+ * Ajax call
+ */
+function loadJSON (url, param) {
   var xmlhttp = new XMLHttpRequest(); 
   xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var data = JSON.parse(this.responseText);
-      // console.log(this.responseText);
-      console.log(data);
+      var html = jsonToTable(data);
+      displayContent(html);
       
     } else if (this.readyState == 4 && this.status != 200) {
       alert("Could not open JSON file");
     }
   };
-  xmlhttp.open("GET", url, true);
+  xmlhttp.open("GET", url+'/'+param, true);
   try{
     xmlhttp.send();
   }catch(e){
     alert("Could not open JSON file");
   }
+}
+
+/*
+ * Generate table by json
+ */
+function jsonToTable(data) {
+
+  var html = "";
+  html += "<table border='1'>";
+  html += "<thead>";
+  html += "<tr>";
+  
+  for(var key in data[0]) {
+    html+="<td>" + key + "</td>";
+  }
+
+  html += "</tr>";
+  html += "</thead>";
+  html += "<tbody>";
+
+  data.forEach(function(row) {
+    html += "<tr>";
+
+    for(var key in row) {
+      var temp = row[key];
+
+      if (key === "links") {
+        var temp2 = "";
+        temp2 += "<ul>";
+        for(var linkKey in temp) {
+          temp2 += "<li>" + temp[linkKey] +"</li>";
+        }
+        temp2 += "<ul>";
+        temp = temp2;
+      }
+
+      html+= "<td>" + temp + "</td>";
+    };
+    html += "</tr>";
+  });
+
+  html += "</tbody>";
+  html += "</table>";
+
+  return html;
+}
+
+/*
+ * Insert the html code to the content
+ */
+function displayContent(html) {
+  document.getElementById("content").innerHTML = html;
 }
